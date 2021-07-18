a16fb6e160806c7ee5fda432bc6d1a212ba16ec2
Andres Saldaña <andres.saldana@wizeline.com>
2021-07-17 21:52:15 -0500

    using mock for feed, and adding login logic

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import tweets from '../../mocks/posts.json';
 
 const Feed = () => (
   <div>
@@ -9,19 +10,34 @@ const Feed = () => (
     </div>
     <div>
       <div>
-        <h2>John Doe @johndoe</h2>
-        <p>Hello world! This is my first tweet, yay!</p>
-        <p>Comments: 0 Likes: 0 Retweets: 0</p>
+        <h2>
+          {tweets[0].author.name} @{tweets[0].author.username}
+        </h2>
+        <p>{tweets[0].content}</p>
+        <p>
+          Comments: {tweets[0].comments} Likes: {tweets[0].retweets} Retweets:{' '}
+          {tweets[0].retweets}
+        </p>
       </div>
       <div>
-        <h2>Cherisse @CherisseCheesse</h2>
-        <p>I like cheese a lot</p>
-        <p>Comments: 0 Likes: 0 Retweets: 2</p>
+        <h2>
+          {tweets[1].author.name} @{tweets[1].author.username}
+        </h2>
+        <p>{tweets[1].content}</p>
+        <p>
+          Comments: {tweets[1].comments} Likes: {tweets[1].retweets} Retweets:{' '}
+          {tweets[1].retweets}
+        </p>
       </div>
       <div>
-        <h2>The Big O @TheRealisticNotation</h2>
-        <p>I like to be realistic.</p>
-        <p>Comments: 0 Likes: 1 Retweets: 0</p>
+        <h2>
+          {tweets[2].author.name} @{tweets[2].author.username}
+        </h2>
+        <p>{tweets[2].content}</p>
+        <p>
+          Comments: {tweets[2].comments} Likes: {tweets[2].retweets} Retweets:{' '}
+          {tweets[2].retweets}
+        </p>
       </div>
     </div>
   </div>
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
 import Feed from '../../components/Feed';
-import user from '../../mocks/user.json';
 import './Profile.css';
-
-const userInfo = () => (
-  <div>
-    <img
-      src={user.background}
-      className="profileBackground"
-      alt="profileBackground"
-    />
-    <img src={user.image} className="profileImage" alt="profileImage" />
-    <h1>{user.name}</h1>
-    <p>{`@${user.username}`}</p>
-    <p>{user.description}</p>
-  </div>
-);
+import ProfileCard from '../../components/ProfileCard';
 
 const Profile = () => (
   <div className="twoSideGrid">
@@ -25,7 +11,7 @@ const Profile = () => (
     </div>
     <div className="flexCentered">
       <div>
-        {userInfo()}
+        <ProfileCard />
         <Feed />
       </div>
     </div>
