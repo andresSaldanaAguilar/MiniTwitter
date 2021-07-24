0f67be771ff3d5755ca98bed8560289a4c015866
Andres Saldaña <andres.saldana@wizeline.com>
2021-07-23 19:47:50 -0500

    Revert "Feat: Haciendo limpieza de código y añadiendo setup para estilos (#2)"
    This reverts commit 0972555d9f445273fba097e13f97fa231a86aff1.

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,31 +1,80 @@
-/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import tweets from '../../mocks/posts.json';
-import Post from '../Post';
-
-export const orderByCreationDate = (a, b) => {
-  const timeA = new Date(a.createdAt).getTime();
-  const timeB = new Date(b.createdAt).getTime();
-  return timeB - timeA;
-};
 
 const Feed = () => (
   <div>
     <div>
-      {tweets.sort(orderByCreationDate).map((tweet) => (
-        <Post
-          key={`tweet-${tweet.id}`}
-          name={tweet.author.name}
-          username={tweet.author.username}
-          avatar={tweet.author.image}
-          content={tweet.content}
-          comments={tweet.comments}
-          likes={tweet.likes}
-          retweets={tweet.retweets}
-          createdAt={tweet.createdAt}
-        />
-      ))}
+      <label>Write somethig: </label>
+      <input />
+    </div>
+    <div>
+      <div>
+        <h2>
+          {tweets[0].author.name} @{tweets[0].author.username}
+        </h2>
+        <p>{tweets[0].content}</p>
+        <p>
+          Comments: {tweets[0].comments} Likes: {tweets[0].retweets} Retweets:{' '}
+          {tweets[0].retweets}
+        </p>
+        <p>Created at: {tweets[2].createdAt}</p>
+      </div>
+      <div>
+        <h2>
+          {tweets[1].author.name} @{tweets[1].author.username}
+        </h2>
+        <p>{tweets[1].content}</p>
+        <p>
+          Comments: {tweets[1].comments} Likes: {tweets[1].retweets} Retweets:{' '}
+          {tweets[1].retweets}
+        </p>
+        <p>Created at: {tweets[2].createdAt}</p>
+      </div>
+      <div>
+        <h2>
+          {tweets[2].author.name} @{tweets[2].author.username}
+        </h2>
+        <p>{tweets[2].content}</p>
+        <p>
+          Comments: {tweets[2].comments} Likes: {tweets[2].retweets} Retweets:{' '}
+          {tweets[2].retweets}
+        </p>
+        <p>Created at: {tweets[2].createdAt}</p>
+      </div>
+      <div>
+        <h2>
+          {tweets[3].author.name} @{tweets[3].author.username}
+        </h2>
+        <p>{tweets[3].content}</p>
+        <p>
+          Comments: {tweets[3].comments} Likes: {tweets[3].retweets} Retweets:{' '}
+          {tweets[3].retweets}
+        </p>
+        <p>Created at: {tweets[3].createdAt}</p>
+      </div>
+      <div>
+        <h2>
+          {tweets[4].author.name} @{tweets[4].author.username}
+        </h2>
+        <p>{tweets[4].content}</p>
+        <p>
+          Comments: {tweets[4].comments} Likes: {tweets[4].retweets} Retweets:{' '}
+          {tweets[4].retweets}
+        </p>
+        <p>Created at: {tweets[4].createdAt}</p>
+      </div>
+      <div>
+        <h2>
+          {tweets[5].author.name} @{tweets[5].author.username}
+        </h2>
+        <p>{tweets[5].content}</p>
+        <p>
+          Comments: {tweets[5].comments} Likes: {tweets[5].retweets} Retweets:{' '}
+          {tweets[5].retweets}
+        </p>
+        <p>Created at: {tweets[5].createdAt}</p>
+      </div>
     </div>
   </div>
 );
